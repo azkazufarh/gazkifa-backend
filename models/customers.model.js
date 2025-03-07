dacb775dabc9ea93e48baa5663cd927ba475c01c
azkazufarh <azkazhanif@gmail.com>
2025-03-07 10:11:53 +0700

    v1.0.8 - delete import in models

--- models/customers.model.js
+++ models/customers.model.js
@@ -1,6 +1,5 @@
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../config/database");
-const Transactions = require("./Transactions.model");
 
 class Customers extends Model {}
 
