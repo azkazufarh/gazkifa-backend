fe535e1656cfc8044e27760ebdfb8ae9d02d9fb5
azkazufarh <azkazhanif@gmail.com>
2025-03-07 10:29:02 +0700

    v.1.0.10 - update cors config

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5174"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
